23519ac77ad2d1b1162c588d3c7afeda62f3c4b6
ermantokatli <senin-github-mailin>
2025-09-26 14:17:12 +0300

    Root ve webhook endpoint eklendi

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
+# Test için root endpoint
 @app.get("/")
 def read_root():
     return {"message": "Railway test başarılı"}
+
+# Webhook test endpoint
+@app.post("/webhook")
+async def webhook(req: Request):
+    data = await req.json()
+    print("Gelen veri:", data)  # loglarda görünecek
+    return {"status": "ok", "received": data}
