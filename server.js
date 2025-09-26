b381044958e60ea85c5bb0dfc240cf877c6a22b5
ermantokatli <senin-github-mailin>
2025-09-26 16:37:29 +0300

    Python dosyaları silindi, Node.js index.html + server.js + package.json eklendi

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.join(__dirname))); // index.html aynı klasörde
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server çalışıyor: http://localhost:${PORT}`);
+});
